2a9eb2dec9901f27dd879409b8065eb5593e3885
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-01-27 11:03:01 +0530

    Updates

---
+++ precache-manifest.98c0cb52b442cbb89422c6196de304e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5912fed9169748c721279d8b9a5aa36",
+    "url": "/pathfinder-react/index.html"
+  },
+  {
+    "revision": "b1bede9b5649163aefe6",
+    "url": "/pathfinder-react/static/css/2.54cdf138.chunk.css"
+  },
+  {
+    "revision": "20235dcedddc4622a577",
+    "url": "/pathfinder-react/static/css/main.e98aee4a.chunk.css"
+  },
+  {
+    "revision": "b1bede9b5649163aefe6",
+    "url": "/pathfinder-react/static/js/2.78ee99e3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pathfinder-react/static/js/2.78ee99e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20235dcedddc4622a577",
+    "url": "/pathfinder-react/static/js/main.6e226581.chunk.js"
+  },
+  {
+    "revision": "0f8f2dace1761ce7cce0",
+    "url": "/pathfinder-react/static/js/runtime-main.00f6bbd3.js"
+  }
+]);
